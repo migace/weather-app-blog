57a15fab80919251b181638d2c4a7c09b028d1b4
Michał Gacek <mgacek@egnyte.com>
2020-04-21 15:40:34 +0200

    Added WeatherInput component, ESlint and Prettier

---
+++ src/WeatherInput.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+export const WeatherInput = ({ searchHandler, searchText = "" }) => {
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+        <div>
+            <input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Enter city name, i.e. Warsaw"
+            />
+            {searchHandler && (
+                <button onClick={(e) => searchHandler(e.target.value)}>
+                    {searchText}
+                </button>
+            )}
+        </div>
+    );
+};
+
+WeatherInput.propTypes = {
+    searchText: PropTypes.string,
+    searchHandler: PropTypes.func,
+};
--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+import noop from "lodash/noop";
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <h1>Hello World!</h1>
+import { WeatherInput } from "./WeatherInput";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("app")
+const App = () => (
+    <>
+        <h1>Hello World!</h1>
+        <WeatherInput searchHandler={noop} searchText="Search" />
+    </>
 );
+
+ReactDOM.render(<App />, document.getElementById("app"));
